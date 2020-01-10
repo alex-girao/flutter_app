c9bbf872d359873d80607ccb8fc990844f6643e6
t0665624 <t0665624@d16396-ssd.sefin.fortaleza.ce.gov.br>
2020-01-10 08:36:28 -0300

    Padding

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,56 +14,58 @@ class HomePage extends StatelessWidget {
 
   _body() {
     return Container(
-      // margin: EdgeInsets.only(left: 10, top: 10),
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _text(),
-          //_img(),
-          _pageView(),
-          _buttons()
-        ],
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        color: Colors.yellow,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _text(),
+            _pageView(),
+            _buttons()
+          ],
+        ),
       ),
     );
   }
 
   Container _pageView() {
     return Container(
-          height: 300,
-          child: PageView(
-            children: <Widget>[
-              _img("assets/images/dog1.png"),
-              _img("assets/images/dog2.png"),
-              _img("assets/images/dog3.png"),
-              _img("assets/images/dog4.png"),
-              _img("assets/images/dog5.png"),
-            ],
-          ),
-        );
+      margin: EdgeInsets.only(top:20, bottom: 20),
+      height: 300,
+      child: PageView(
+        children: <Widget>[
+          _img("assets/images/dog1.png"),
+          _img("assets/images/dog2.png"),
+          _img("assets/images/dog3.png"),
+          _img("assets/images/dog4.png"),
+          _img("assets/images/dog5.png"),
+        ],
+      ),
+    );
   }
 
   _buttons() {
     return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _button("ListView"),
-                _button("Page 2"),
-                _button("page 3")
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                _button("Saback"),
-                _button("Dialog"),
-                _button("Toast")
-              ],
-            )
+            _button("ListView"),
+            _button("Page 2"),
+            _button("page 3")
           ],
-        );
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _button("Saback"),
+            _button("Dialog"),
+            _button("Toast")
+          ],
+        )
+      ],
+    );
   }
 
   _button(String text) {
@@ -88,7 +89,6 @@ class HomePage extends StatelessWidget {
 
   _img(String img) {
     return Container(
-      margin: EdgeInsets.all(20),
       child: Image.asset(
         img,
         fit: BoxFit.cover,
@@ -106,7 +106,7 @@ class HomePage extends StatelessWidget {
         fontStyle: FontStyle.italic,
         decoration: TextDecoration.underline,
         decorationColor: Colors.red,
-        decorationStyle: TextDecorationStyle.dotted,
+        decorationStyle: TextDecorationStyle.wavy,
       ),
     );
   }
