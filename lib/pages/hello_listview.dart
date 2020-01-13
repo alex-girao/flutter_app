ea5c2eb1a32ddfd67fc1fb0578749de09367866a
Alex Girão <fco.alex.girao@gmail.com>
2020-01-12 23:46:03 -0300

    Alterando o estado do componente

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -7,12 +7,42 @@ class Dog {
   Dog(this.nome, this.foto);
 }
 
-class HelloListView extends StatelessWidget {
+/**
+ *  Os Widgets que herdam de StatefulWidget podem alterar o prórpio estado
+ *  de acordo com eventos prefinidos
+ */
+class HelloListView extends StatefulWidget {
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  // variaveis que começam com underline são privadas
+  bool _gridView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ListView'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {
+              setState(() {
+                _gridView = false;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
+              setState(() {
+                _gridView = true;
+              });
+            },
+          ),
+        ],
       ),
       body: _body(),
     );
@@ -26,46 +56,51 @@ class HelloListView extends StatelessWidget {
       Dog("Rottweiler", "assets/images/dog4.png"),
       Dog("Pastor", "assets/images/dog5.png"),
     ];
-    return ListView.builder(
-      itemCount: dogs.length,
-      itemExtent: 300,
-      itemBuilder: (BuildContext context, int index) {
-        Dog dog = dogs[index];
-        //return _img(dog.foto);
-        return Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            //primeiro a imagem
-            _img(dog.foto),
-            //depois o nome
-            Align(
-              alignment: Alignment.topLeft,
-              //alignment: Alignment(1,1),
-              child: Container(
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.circular(16)
-                ),
-                child: Text(
-                  dog.nome,
-                  style: TextStyle(fontSize: 26, color: Colors.white),
-                ),
-              ),
+    if (_gridView) {
+      return GridView.builder(
+        gridDelegate:
+        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: dogs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _itemView(dogs, index);
+        },
+      );
+    } else {
+      return ListView.builder(
+        itemExtent: 350,
+        itemCount: dogs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _itemView(dogs, index);
+        },
+      );
+    }
+  }
+
+  Stack _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index];
+    //return _img(dog.foto);
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        //primeiro a imagem
+        _img(dog.foto),
+        //depois o nome
+        Align(
+          alignment: Alignment.topLeft,
+          //alignment: Alignment(1,1),
+          child: Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.circular(16)),
+            child: Text(
+              dog.nome,
+              style: TextStyle(fontSize: 26, color: Colors.white),
             ),
-          ],
-        );
-        /* pode substituir o stack
-        return Stack(
-          children: <Widget>[
-            SizedBox.expand(
-              child: _img(dog.foto),
-            )
-          ],
-        );
-        */
-      },
+          ),
+        ),
+      ],
     );
   }
 
